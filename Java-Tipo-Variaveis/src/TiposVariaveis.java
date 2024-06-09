463f01c989b826f28ed3b0509b2f4670aad2b392
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2024-06-09 10:14:34 -0300

    feat: Adição de práticas com tipos e variáveis em Java

---
+++ Java-Tipo-Variaveis/src/TiposVariaveis.java
@@ -0,0 +1,23 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        // Tipos e Variáveis
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        // Constante
+        double VALOR_DE_PI = 3.14;
+
+        VALOR_DE_PI = 10.75;
+
+        final double VALOR_DE_PI2 = 3.14;
+
+        // VALOR_DE_PI2 = 10.75;
+    }
+}
