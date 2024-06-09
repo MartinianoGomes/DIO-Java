f44dbf8e251d9f292ae09252b4658fade4440114
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2024-06-09 11:16:02 -0300

    feat: Adicionando classe SmartTV da API

---
+++ SmartTVAPI/src/SmartTV.java
@@ -0,0 +1,35 @@
+public class SmartTV {
+    private Boolean ligada;
+    private Integer canal;
+    private Integer volume;
+
+    public void SmartTV(Boolean ligada, Integer canal, Integer volume) {
+        this.ligada = ligada;
+        this.canal = canal;
+        this.volume = volume;
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void subirCanal() {
+        canal++;
+    }
+
+    public void descerCanal() {
+        canal--;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+}
