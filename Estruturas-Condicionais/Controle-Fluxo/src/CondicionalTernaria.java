c26e58c10b812190bf6500cbf75d57413bccc4de
Martiniano Gomes <uricanooriental@gmail.com>
2024-06-19 18:12:21 -0300

    feat: Condição Ternária.

---
+++ Estruturas-Condicionais/Controle-Fluxo/src/CondicionalTernaria.java
@@ -0,0 +1,13 @@
+public class CondicionalTernaria {
+	public static void main(String[] args) {
+        // Cenário 1
+		int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+
+        // Cenário 2
+        int nota2 = 6;
+		String resultado2 = nota2 >=7 ? "Aprovado" : nota2 >=5 && nota2 <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado2);
+	}
+}
