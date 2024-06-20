0f8c947928c1a97163bff476a8ccf6dd6e9ad7ce
Martiniano Gomes <uricanooriental@gmail.com>
2024-06-20 17:06:17 -0300

    feat: Praticando codicionais com Java.

---
+++ Estruturas-Java/Controle-Fluxo/src/ProcessoSeletivo.java
@@ -0,0 +1,17 @@
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        
+    }
+
+    static void analisarCandidato(Double salarioPretendido) {
+        Double salarioBase = 2000.0;
+
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+}
