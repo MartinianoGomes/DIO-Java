1c15f03ad225fd8645254dedd10a9b36ede0c9fe
Martiniano Gomes <uricanooriental@gmail.com>
2024-06-24 22:18:40 -0300

    feat: Codando o projeto e organizando pastas.

---
+++ Fundamentos-POO/Escola/src/Escola.java
@@ -0,0 +1,13 @@
+//arquivo Escola.java
+
+import com.escola.turma.Aluno;
+
+public class Escola {
+	public static void main(String[] args) {
+		Aluno felipe = new Aluno();
+		felipe.setNome("Felipe");
+		felipe.setIdade(8);
+		
+		System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");	
+	}
+}
